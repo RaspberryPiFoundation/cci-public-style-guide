f389ec4a41b19a9ce9561640cdc1c578f118a59a
Jonic Linley <jonic@100yen.co.uk>
2015-10-30 15:42:28 +0000

    Disabled asset pipeline on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # Do not compress assets using the Asset Pipeline
+  config.assets.enabled = false
+
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
