2ad4d500078f0ddc386272bb75e5d9cea2884f41
Jonic Linley <jonic@100yen.co.uk>
2015-11-14 04:05:27 +0000

    Added application css and js

---
+++ public/assets/javascripts/application.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * application.js
+ *
+ *= require_self
+ */
+
+$(document).ready(function () {
+  $('.toggle_drawer').click(function () {
+    $('body').toggleClass('drawer_toggle');
+    return false;
+  });
+});
+//# sourceMappingURL=application.js.map
