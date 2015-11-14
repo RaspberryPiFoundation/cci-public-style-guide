2c94f9594b8afb4501bddb6d455d773e494d631e
Jonic Linley <jonic@100yen.co.uk>
2015-11-14 02:48:01 +0000

    ApplicationHelper overrides assets helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,32 @@ module ApplicationHelper
     "page page--#{@page}"
   end
 
+  def stylesheet_link_tag(url, options = {})
+    super(asset_url(url), options)
+  end
+
+  def crossorigin_javascript_include_tag(url, options = {})
+    super(asset_url(url), options)
+  end
+
+  def javascript_include_tag(url, options = {})
+    super(asset_url(url), options)
+  end
+
+  def image_tag(url, options = {})
+    super(asset_url(url), options)
+  end
+
+  def image_path(url, options = {})
+    super(asset_url(url), options)
+  end
+
+  def image_url(url, options = {})
+    super((ActionController::Base.asset_host || "") + asset_url(url), options)
+  end
+
+  def asset_url(url)
+    url = AssetManifest.asset_path(url)
+  end
+
 end
