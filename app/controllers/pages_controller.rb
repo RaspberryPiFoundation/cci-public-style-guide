5fbf962b4160fd5cb659e6c6ab5bb90a67552ead
Jonic Linley <jonic@100yen.co.uk>
2015-11-17 00:22:19 +0000

    Pages#show now renders based on reorganised page partials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
 
   def home
+    #@page = Page.find('foo', 'bar')
     @page = 'home'
   end
 
   def show
-    @page = params[:page]
-    render @page
+    @section = params[:section]
+    @page    = params[:page]
+
+    render "pages/#{@section}/#{@page}"
   end
 
 end
