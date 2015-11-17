d23eb8edada9611c3dec6d3cd6fdda98c1dd7488
Jonic Linley <jonic@100yen.co.uk>
2015-11-17 00:21:54 +0000

    Application helper makes cc_config available across all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_cc_config
+
+  private
+
+  def set_cc_config
+    @cc_config = CC_CONFIG
+  end
+
 end
