2f7482af5cfc85ef8d0a58f20c02b9f639a89ac4
Jonic Linley <jonic@100yen.co.uk>
2015-11-18 19:20:56 +0000

    Refactored page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   get '/example_pages/:page' => 'example_pages#show', :as => 'example_pages_show'
-  get '/:section/:page'      => 'pages#show',         :as => 'pages_show'
-  get '/'                    => 'pages#home',         :as => 'home'
+
+  get '/:section/:page' => 'pages#show',    :as => 'pages_show'
+
+  get '/' => 'pages#show', :as => 'home', defaults: { page: 'home' }
 
 end
