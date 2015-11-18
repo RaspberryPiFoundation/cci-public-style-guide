e5b66ffc447d3a9f60dee595aa57480b3b7be46d
Jonic Linley <jonic@100yen.co.uk>
2015-11-18 19:23:02 +0000

    Added sections route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get '/example_pages/:page' => 'example_pages#show', :as => 'example_pages_show'
 
+  get '/:section'       => 'sections#show', :as => 'sections_show'
   get '/:section/:page' => 'pages#show',    :as => 'pages_show'
 
   get '/' => 'pages#show', :as => 'home', defaults: { page: 'home' }
