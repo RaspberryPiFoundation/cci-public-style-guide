cc98ede333ec4a51cf7cd8d455d29f217b47d9c5
Jonic Linley <jonic@100yen.co.uk>
2015-11-19 17:24:11 +0000

    Pages check for existence of view, 404s if not found

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < ApplicationController
 
-  before_filter :set_path
-  before_filter :set_page
+  before_filter :set_page_path,     :only => [:show]
+  before_filter :set_page,          :only => [:show]
+  before_filter :set_page_has_view, :only => [:show]
+
+  def show
+    if @page_has_view
+      return render :show
+    end
+
+    # Still here? 404.
+    return not_found
+  end
 
   private
 
@@ -9,12 +19,12 @@ class PagesController < ApplicationController
     @page = Page.where(:path => @page_path).first || not_found
   end
 
-  def set_path
-    if params[:section].present?
-      @page_path = [params[:section], params[:page]].join('/')
-    else
-      @page_path = params[:page]
-    end
+  def set_page_has_view
+    @page_has_view = File.exists?(Rails.root.join("app", "views", "pages", "content", "#{@page.section.path}", "_#{@page_path}.html.erb"))
+  end
+
+  def set_page_path
+    @page_path = params[:page]
   end
 
 end
