81411501570be09c560092c6583d3ca9d109dbf0
Jonic Linley <jonic@100yen.co.uk>
2015-11-19 17:25:17 +0000

    Added "shown_in_nav" scope to Hideable concern

--- app/models/concerns/hideable.rb
+++ app/models/concerns/hideable.rb
@@ -4,6 +4,8 @@ module Hideable
   included do
     before_create :set_default_values
 
+    scope :shown_in_nav, -> { where(:show_in_nav => true) }
+
     def set_default_values
       self.show_in_nav = true if show_in_nav.nil?
     end
