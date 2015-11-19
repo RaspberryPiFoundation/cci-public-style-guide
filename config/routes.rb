2b1baf4c84f840586166271dd7845e14e4bfffd3
Jonic Linley <jonic@100yen.co.uk>
2015-11-19 16:59:30 +0000

    Excluded resources for unused admin sections and pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get '/admin' => 'admin#index', :as => 'admin'
 
   namespace :admin do
-    resources :pages,    :except => [:show]
-    resources :sections, :except => [:show]
+    resources :pages,    :except => [:index, :show]
+    resources :sections, :except => [:index, :show]
   end
 
   get '/example_pages/:page' => 'example_pages#show', :as => 'example_page'
