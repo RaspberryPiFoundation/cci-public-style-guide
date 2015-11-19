bb3685c1b524d30b21d719cb695a7c82a8510fcb
Jonic Linley <jonic@100yen.co.uk>
2015-11-19 17:25:00 +0000

    Sections check for partial, shows if found, redirects to its first page if not, 404s if neither work

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,15 +1,32 @@
 class SectionsController < ApplicationController
 
-  before_filter :set_section, :only => [:show]
+  before_filter :set_section_path,     :only => [:show]
+  before_filter :set_section,          :only => [:show]
+  before_filter :set_section_has_view, :only => [:show]
 
   def show
-    redirect_to pages_show_path(@section.path, @section.pages.first.path)
+    if @section_has_view
+      return render :show
+    elsif @section.pages.any?
+      return redirect_to page_path(@section.path, @section.pages.first.path)
+    end
+
+    # Still here? 404.
+    not_found
   end
 
   private
 
   def set_section
-    @section = Section.where(:path => params[:section]).first || not_found
+    @section = Section.where(:path => @section_path).first || not_found
+  end
+
+  def set_section_path
+    @section_path = params[:section]
+  end
+
+  def set_section_has_view
+    @section_has_view = File.exists?(Rails.root.join("app", "views", "sections", "content", "_#{@section_path}.html.erb"))
   end
 
 end
