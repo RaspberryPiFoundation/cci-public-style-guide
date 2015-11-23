6ee68d6b7c4d6149dca447eee15ede0f1ff04516
Jonic Linley <jonic@100yen.co.uk>
2015-11-23 17:44:12 +0000

    Component helper can now render code for variants, code with a component via a method option, or code for a component all by itself

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -1,16 +1,27 @@
 module ComponentHelper
 
   def ui_variants(name)
-    file_path        = "#{Rails.root}/config/_components/#{name}.yml"
-    component_config = YAML.load(File.read(file_path)).with_indifferent_access
+    component_config = get_component_config_for(name)
 
-    render :partial => "shared/components_variants", :locals => component_config
+    render :partial => "shared/component_variants", :locals => component_config
   end
 
-  def ui(name, options = {}, &block)
+  def ui_variants_code(name)
+    component_config = get_component_config_for(name)
+
+    render :partial => "shared/component_variants_code", :locals => component_config
+  end
+
+  def ui(name, options = {}, show_code = false, &block)
     options = prepare_options!(name, options, &block)
 
-    render :partial => "_components/#{name}",   :locals => options
+    html_buffer = render :partial => "_components/#{name}", :locals => options
+
+    if show_code
+      html_buffer << ui_code(name, options, &block)
+    end
+
+    return html_buffer
   end
 
   def ui_code(name, options, &block)
@@ -26,6 +37,11 @@ module ComponentHelper
       :content      => nil,
       :html_classes => nil
     }
+  end
+
+  def get_component_config_for(name)
+    file_path        = "#{Rails.root}/config/_components/#{name}.yml"
+    component_config = YAML.load(File.read(file_path)).with_indifferent_access
   end
 
   def prepare_options!(name, options, &block)
