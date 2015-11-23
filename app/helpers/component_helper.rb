a0bff0bafd01584da2bfbf303dd9689f0d88df06
Jonic Linley <jonic@100yen.co.uk>
2015-11-23 16:10:02 +0000

    Component helper can render individual components, or collections of components variants based on component config

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -1,13 +1,38 @@
 module ComponentHelper
 
-  def ui(name, options = {}, &block)
-    buffer = capture(&block) if block_given?
+  def ui_variants(name)
+    file_path        = "#{Rails.root}/config/_components/#{name}.yml"
+    component_config = YAML.load(File.read(file_path)).with_indifferent_access
 
-    @html = ""
-    @html << "START OF #{name}<br>"
-    @html << buffer if buffer.present?
-    @html << "END OF #{name}"
-    @html.html_safe
+    render :partial => "shared/components_variants", :locals => component_config
+  end
+
+  def ui(name, options = {}, &block)
+    options = prepare_options!(name, options, &block)
+
+    render :partial => "_components/#{name}",   :locals => options
+  end
+
+  def ui_code(name, options, &block)
+    options = prepare_options!(name, options, &block)
+
+    render :partial => "shared/component_code", :locals => { :name => name, :options => options }
+  end
+
+  private
+
+  def default_ui_options
+    {
+      :content      => nil,
+      :html_classes => nil
+    }
+  end
+
+  def prepare_options!(name, options, &block)
+    options[:name]    = name
+    options[:content] = capture(&block) if block_given?
+
+    default_ui_options.merge(options).with_indifferent_access
   end
 
 end
