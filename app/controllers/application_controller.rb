11ba739ea4ff3454734bdb97b81ebf36ad1f5c04
Jonic Linley <jonic@100yen.co.uk>
2015-11-30 00:19:38 +0000

    Edited admin authentication to use helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,19 @@
+include AuthHelper
+
 class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :authenticate_user!
-
   private
-
-  def authenticate_user!
-    Rails.logger.info('before_filter :authenticate_user!')
-    # if no session
-    #   redirect to login
-    # end
-  end
 
   def authenticate_admin_user!
     Rails.logger.info('before_filter :authenticate_admin_user!')
 
-    authenticate_user!
-
-    # if user is not admin
-    #
-    # end
+    if !user_is_authenticated?
+      redirect_to :admin_login
+    end
   end
 
   def not_found
