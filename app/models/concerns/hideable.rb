50f6c669e8d161736ec1a1cbe24178c73ca6ca68
Jonic Linley <jonic@100yen.co.uk>
2015-11-30 00:20:41 +0000

    Added `hidden_from_nav` scope to Hideable model concern

--- app/models/concerns/hideable.rb
+++ app/models/concerns/hideable.rb
@@ -4,7 +4,8 @@ module Hideable
   included do
     before_create :set_default_values
 
-    scope :shown_in_nav, -> { where(:show_in_nav => true) }
+    scope :shown_in_nav,    -> { where(:show_in_nav => true)  }
+    scope :hidden_from_nav, -> { where(:show_in_nav => false) }
 
     def set_default_values
       self.show_in_nav = true if show_in_nav.nil?
