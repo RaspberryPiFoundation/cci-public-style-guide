96469e5c0f9af9a208d6d3c76a75adbce90ebe22
Jonic Linley <jonic@100yen.co.uk>
2015-11-30 00:19:55 +0000

    Created admin controller, extends application

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,15 @@
 class AdminController < ApplicationController
 
+  before_filter :authenticate_admin_user!, :except => [:login]
+
   def index
     @sections = Section.all
+  end
+
+  def login
+    if user_is_authenticated?
+      redirect_to :admin_index
+    end
   end
 
 end
