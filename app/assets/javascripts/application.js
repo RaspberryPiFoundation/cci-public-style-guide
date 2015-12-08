55a887dae359407be743ad35f9e9127a5a315dd0
Greg Wood <gregw@codeclub.org.uk>
2015-12-08 14:53:29 +0000

    listing styles mergin in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,8 +5,15 @@
  */
 
 $( document ).ready(function() {
+
  $('.toggle_drawer').click(function() {
    $('body').toggleClass('drawer_toggle');
    return false;
  });
+
+ $('.toggle').click(function() {
+  $(this).next().toggleClass('hidden');
+  $(this).toggleClass('down');
+ });
+
 });
