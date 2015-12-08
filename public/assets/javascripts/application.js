90b50ede516a5bf14482c832615edb0d806c62b1
Jonic Linley <jonic@100yen.co.uk>
2015-12-08 21:04:32 +0000

    Generated public assets following merge

--- public/assets/javascripts/application.js
+++ public/assets/javascripts/application.js
@@ -2,7 +2,7 @@
  * application.js
  * 
  * Code Club Style Guide - A collection of UI components for styling Code Club websites
- * @version v0.0.1-0
+ * @version v0.0.0
  * @link https://styleguide.codeclubworld.org/
  */
 'use strict';
@@ -14,9 +14,15 @@
  */
 
 $(document).ready(function () {
+
   $('.toggle_drawer').click(function () {
     $('body').toggleClass('drawer_toggle');
     return false;
+  });
+
+  $('.toggle').click(function () {
+    $(this).next().toggleClass('hidden');
+    $(this).toggleClass('down');
   });
 });
 //# sourceMappingURL=application.js.map
