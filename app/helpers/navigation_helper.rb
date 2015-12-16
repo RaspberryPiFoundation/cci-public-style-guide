83ea1701fac0fd4f2ea7685cff8b1057579aee3b
Jonic Linley <jonic@100yen.co.uk>
2015-12-16 22:21:17 +0000

    Updated markup in navigation helper

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,11 +1,13 @@
 module NavigationHelper
 
   def render_navigation
-    @html = ""
+    @html = '<nav class="sg-navigation" role="navigation">'
 
     Section.shown_in_nav.each do |section|
       make_navigation_section(section)
     end
+
+    @html << '</nav>'
 
     @html.html_safe
   end
@@ -13,7 +15,7 @@ module NavigationHelper
   private
 
   def make_navigation_section(section)
-    @html << content_tag(:h5, section.title)
+    @html << content_tag(:p, section.title, :class => 'sg-navigation-list-header u-h5')
 
     make_navigation_pages_list(section)
 
@@ -24,12 +26,16 @@ module NavigationHelper
     list_items = ""
 
     section.pages.shown_in_nav.each do |page|
-      list_items << content_tag(:li, :class => current_page_class(section.path, page.path)) do
-        link_to(page.title, page_path(section.path, page.path))
+
+      list_items << content_tag(:li, :class => 'sg-navigation-item') do
+        link_class = 'sg-navigation-link'
+        link_class << current_page_class(section.path, page.path)
+
+        link_to(page.title, page_path(section.path, page.path), :class => link_class)
       end
     end
 
-    @html << content_tag(:ul) do
+    @html << content_tag(:ul, :class => 'sg-navigation-list') do
       list_items.html_safe
     end
 
@@ -37,9 +43,13 @@ module NavigationHelper
   end
 
   def current_page_class(section, page)
+    page_class = ''
+
     if @section.present? && @page.present?
-      'cur' if section == @section.path && page == @page.path
+      page_class = ' sg-current-page' if section == @section.path && page == @page.path
     end
+
+    page_class
   end
 
 end
