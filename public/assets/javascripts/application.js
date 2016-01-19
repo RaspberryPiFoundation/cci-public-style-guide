90d17fc019ab4b2d83580bf3311fc3bec6529d88
Jonic Linley <jonic@100yen.co.uk>
2016-01-19 11:49:04 +0000

    Removed unused application javascript

--- public/assets/javascripts/application.js
+++ public/assets/javascripts/application.js
@@ -2,27 +2,8 @@
  * application.js
  * 
  * Code Club Style Guide - A collection of UI components for styling Code Club websites
- * @version v0.0.1-0
+ * @version v0.0.1-6
  * @link https://styleguide.codeclubworld.org/
  */
-'use strict';
-
-/**
- * application.js
- *
- *= require_self
- */
-
-$(document).ready(function () {
-
-  $('.toggle_drawer').click(function () {
-    $('body').toggleClass('drawer_toggle');
-    return false;
-  });
-
-  $('.toggle').click(function () {
-    $(this).next().toggleClass('hidden');
-    $(this).toggleClass('down');
-  });
-});
+"use strict";
 //# sourceMappingURL=application.js.map
