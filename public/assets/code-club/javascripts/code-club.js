92d6a25a04679712609d380fb246f947c2f69993
Jonic Linley <jonic@100yen.co.uk>
2016-04-25 18:22:54 +0000

    Committing changes for v1.0.13

--- public/assets/code-club/javascripts/code-club.js
+++ public/assets/code-club/javascripts/code-club.js
@@ -2,7 +2,7 @@
  * code-club.js
  * 
  * Code Club Style Guide - A collection of UI components for styling Code Club websites
- * @version v1.0.12
+ * @version v1.0.13
  * @link https://styleguide.codeclubworld.org/
  */
 //  Import Modules from component JavaScript files
