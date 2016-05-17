119652aaf930cf8d3254e91fd316e7e0d386449c
Jonic Linley <jonic@100yen.co.uk>
2016-05-17 14:50:57 +0100

    Removed Rails assets paths we no longer need

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,11 +2,3 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.1'
-
-# Add additional assets to the asset load path
-Rails.application.config.assets.paths << "#{Rails.root}/public/assets/code-club/images"
-Rails.application.config.assets.paths << "#{Rails.root}/public/assets/code-club/images/favicons"
-Rails.application.config.assets.paths << "#{Rails.root}/public/assets/code-club/images/icons"
-Rails.application.config.assets.paths << "#{Rails.root}/public/assets/code-club/images/project-cards"
-Rails.application.config.assets.paths << "#{Rails.root}/public/assets/code-club/javascripts"
-Rails.application.config.assets.paths << "#{Rails.root}/public/assets/code-club/stylesheets"
