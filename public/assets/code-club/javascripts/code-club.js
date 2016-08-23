dab9d18c0ccf4cb7c1aea126bc823a2d5277e979
Yulia Bulgakova <yulia_bulgakova@hotmail.com>
2016-08-23 14:10:16 +0000

    Committing changes for v1.0.59

--- public/assets/code-club/javascripts/code-club.js
+++ public/assets/code-club/javascripts/code-club.js
@@ -2,7 +2,7 @@
  * code-club.js
  * 
  * Code Club Style Guide - A collection of UI components for styling Code Club websites
- * @version v1.0.58
+ * @version v1.0.59
  * @link https://styleguide.codeclubworld.org/
  */
 //  Import Modules from component JavaScript files
