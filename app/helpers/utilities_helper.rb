975d71308e6d66eb1af3252ec0124aa9ecef5d24
Yulia Bulgakova <yulia_bulgakova@hotmail.com>
2016-08-30 12:14:22 +0100

    Remove visual examples of margin and padding #159

--- app/helpers/utilities_helper.rb
+++ app/helpers/utilities_helper.rb
@@ -1,15 +1,39 @@
 module UtilitiesHelper
 
-  UTILITIES_PATH = "app/assets/code-club/stylesheets/utilities/"
+  BASE = 16
 
   def extract_classes_from_file(file_name)
-
-    file = File.open(UTILITIES_PATH + file_name, "r")
-    data = file.read
-    file.close
-
+    data = File.read(File.join(Rails.root, 'app', 'assets', 'code-club', 'stylesheets', 'utilities', file_name))
     matches = data.to_enum(:scan, /u-[a-zA-Z0-9|-]+*/).map { Regexp.last_match }
     matches.map { |match| match[0] }.uniq
+  end
+
+
+  def set_margin_classes
+    @class_prefixes = ['.u-margin--', '.u-margin-top--', '.u-margin-right--', '.u-margin-bottom--', '.u-margin-left--']
+    @class_values =  [ ['none', 0],
+                       ['base', BASE],
+                       ['xx-small', BASE * 0.25],
+                       ['x-small',  BASE * 0.5],
+                       ['small', BASE * 0.75],
+                       ['large', BASE * 2],
+                       ['x-large', BASE * 3],
+                       ['xx-large', BASE * 4]]
+
+  end
+
+  def set_padding_classes
+    @class_prefixes = ['.u-padding--', '.u-padding-top--', '.u-padding-right--', '.u-padding-bottom--', '.u-padding-left--']
+    @class_values =  [ ['none', 0],
+                       ['base', BASE],
+                       ['xx-small', BASE * 0.25],
+                       ['x-small',  BASE * 0.5],
+                       ['small', BASE * 0.75],
+                       ['large', BASE * 1.25],
+                       ['x-large', BASE * 2],
+                       ['xx-large', BASE * 3],
+                       ['xxx-large', BASE * 4],
+                       ['xxxx-large', BASE * 5]]
   end
 
 end
