96bf1eb7f034217f132d8837cabeb6277813ceb1
Yulia Bulgakova <yulia_bulgakova@hotmail.com>
2016-09-02 14:49:35 +0100

    Improve margin and padding pages and fix indentations

--- app/helpers/utilities_helper.rb
+++ app/helpers/utilities_helper.rb
@@ -1,5 +1,6 @@
 module UtilitiesHelper
 
+
   BASE = 16
 
   def extract_classes_from_file(file_name)
@@ -9,31 +10,37 @@ module UtilitiesHelper
   end
 
 
-  def set_margin_classes
-    @class_prefixes = ['.u-margin--', '.u-margin-top--', '.u-margin-right--', '.u-margin-bottom--', '.u-margin-left--']
-    @class_values =  [ ['none', 0],
-                       ['base', BASE],
-                       ['xx-small', BASE * 0.25],
-                       ['x-small',  BASE * 0.5],
-                       ['small', BASE * 0.75],
-                       ['large', BASE * 2],
-                       ['x-large', BASE * 3],
-                       ['xx-large', BASE * 4]]
-
+  def get_location_prefixes_for(attribute)
+    ['.u-' + attribute + '--',
+     '.u-' + attribute + '-top--',
+     '.u-' + attribute + '-right--',
+     '.u-' + attribute + '-bottom--',
+     '.u-' + attribute + '-left--']
   end
 
-  def set_padding_classes
-    @class_prefixes = ['.u-padding--', '.u-padding-top--', '.u-padding-right--', '.u-padding-bottom--', '.u-padding-left--']
-    @class_values =  [ ['none', 0],
-                       ['base', BASE],
-                       ['xx-small', BASE * 0.25],
-                       ['x-small',  BASE * 0.5],
-                       ['small', BASE * 0.75],
-                       ['large', BASE * 1.25],
-                       ['x-large', BASE * 2],
-                       ['xx-large', BASE * 3],
-                       ['xxx-large', BASE * 4],
-                       ['xxxx-large', BASE * 5]]
+
+  def get_margin_classes
+    [['none', 0],
+     ['base', BASE],
+     ['xx-small', BASE * 0.25],
+     ['x-small',  BASE * 0.5],
+     ['small', BASE * 0.75],
+     ['large', BASE * 2],
+     ['x-large', BASE * 3],
+     ['xx-large', BASE * 4]]
+  end
+
+  def get_padding_classes
+    [['none', 0],
+     ['base', BASE],
+     ['xx-small', BASE * 0.25],
+     ['x-small',  BASE * 0.5],
+     ['small', BASE * 0.75],
+     ['large', BASE * 1.25],
+     ['x-large', BASE * 2],
+     ['xx-large', BASE * 3],
+     ['xxx-large', BASE * 4],
+     ['xxxx-large', BASE * 5]]
   end
 
 end
